ee3b2a9d3c74d0a5cd9c31744caab20c5b9d50e7
WinterThane <demonsaso@gmail.com>
2017-09-07 19:17:00 +0200

    Convert up to 39
    Added conversion up to 39.
    Added units tests.

--- ConvertToRoman/ConvertToRoman.cs
+++ ConvertToRoman/ConvertToRoman.cs
@@ -13,13 +13,28 @@ namespace ConvertToRoman
             //Input values are known to be in the range 1..3999.
             if (input <= 0 || input > 3999)
                 throw new ArgumentOutOfRangeException("Roman numerals are only positive numbers between 1 and 3999.");
-           
+
             int remainder = input;
 
             //Roman numerals consist of the letters I (1), V (5), X (10), L (50), C (100), D (500), M (1000)
             while (remainder > 0)
             {
-                if (remainder >= 4)
+                if (remainder >= 10)
+                {
+                    result.Append("X");
+                    remainder -= 10;
+                }
+                else if (remainder >= 9)
+                {
+                    result.Append("IX");
+                    remainder -= 9;
+                }
+                else if (remainder >= 5)
+                {
+                    result.Append("V");
+                    remainder -= 5;
+                }
+                else if (remainder >= 4)
                 {
                     result.Append("IV");
                     remainder -= 4;
--- ConvertToRomanTests/ConvertToRomanTests.cs
+++ ConvertToRomanTests/ConvertToRomanTests.cs
@@ -30,5 +30,65 @@ namespace ConvertToRoman.Tests
         {
             Assert.AreEqual("IV", test.ToRomanNumber(4));
         }
+
+        [TestMethod()]
+        public void ToRoman_5()
+        {
+            Assert.AreEqual("V", test.ToRomanNumber(5));
+        }
+
+        [TestMethod()]
+        public void ToRoman_6()
+        {
+            Assert.AreEqual("VI", test.ToRomanNumber(6));
+        }
+
+        [TestMethod()]
+        public void ToRoman_7()
+        {
+            Assert.AreEqual("VII", test.ToRomanNumber(7));
+        }
+
+        [TestMethod()]
+        public void ToRoman_8()
+        {
+            Assert.AreEqual("VIII", test.ToRomanNumber(8));
+        }
+
+        [TestMethod()]
+        public void ToRoman_9()
+        {
+            Assert.AreEqual("IX", test.ToRomanNumber(9));
+        }
+
+        [TestMethod()]
+        public void ToRoman_10()
+        {
+            Assert.AreEqual("X", test.ToRomanNumber(10));
+        }
+
+        [TestMethod()]
+        public void ToRoman_11()
+        {
+            Assert.AreEqual("XI", test.ToRomanNumber(11));
+        }
+
+        [TestMethod()]
+        public void ToRoman_14()
+        {
+            Assert.AreEqual("XIV", test.ToRomanNumber(14));
+        }
+
+        [TestMethod()]
+        public void ToRoman_15()
+        {
+            Assert.AreEqual("XV", test.ToRomanNumber(15));
+        }
+
+        [TestMethod()]
+        public void ToRoman_39()
+        {
+            Assert.AreEqual("XXXIX", test.ToRomanNumber(39));
+        }
     }
 }
